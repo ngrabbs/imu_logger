476bae68035341b4594d695ce7b6abc0db0010f4
Nick <ngrabbs@gmail.com>
2022-08-18 03:30:36 +0100

    add 6dof memlog and file write

--- code.py
+++ code.py
@@ -1,16 +1,49 @@
 import board
 import digitalio
+import displayio
+import terminalio
 import time
 from adafruit_debouncer import Debouncer
 from adafruit_datetime import datetime
 
-SWITCH_PIN = board.A1
+########### BEGIN IMU ##############
+from adafruit_lsm6ds import Rate, AccelRange, GyroRange
+#from adafruit_lsm6ds.lsm6dsox import LSM6DSOX as LSM6DS
+from adafruit_lsm6ds.lsm6ds33 import LSM6DS33 as LSM6DS
+# from adafruit_lsm6ds.lsm6dso32 import LSM6DSO32 as LSM6DS
+# from adafruit_lsm6ds.ism330dhcx import ISM330DHCX as LSM6DS
+
+i2c = board.I2C()
+sensor = LSM6DS(i2c)
+sensor.accelerometer_range = AccelRange.RANGE_8G
+sensor.gyro_range = GyroRange.RANGE_2000_DPS
+sensor.accelerometer_data_rate = Rate.RATE_1_66K_HZ
+# sensor.accelerometer_data_rate = Rate.RATE_12_5_HZ
+sensor.gyro_data_rate = Rate.RATE_1_66K_HZ
+########### END IMU ##############
+
+########### BUTTON ###############
+#SWITCH_PIN = board.A1
+SWITCH_PIN = board.D6
 RECORD_TIME = 10
 
 switch_io = digitalio.DigitalInOut(SWITCH_PIN)
 switch_io.direction = digitalio.Direction.INPUT
 switch_io.pull = digitalio.Pull.UP
 switch = Debouncer(switch_io)
+########### END BUTTON ###########
+
+########### DISPLAY ##############
+from adafruit_display_text import label
+import adafruit_displayio_ssd1306
+displayio.release_displays()
+display_bus = displayio.I2CDisplay(i2c, device_address=0x3C)
+display = adafruit_displayio_ssd1306.SSD1306(display_bus, width=128, height=32)
+
+# Make the display context
+splash = displayio.Group()
+display.show(splash)
+########### END DISPLAY ##########
 
 ###### Helper Functions ######
 
@@ -48,7 +81,7 @@ class StateMachine(object):
 
     def update(self):
         if self.state:
-            print('Updating %s' % (self.state.name))
+#            print('Updating %s' % (self.state.name))
             self.state.update(self)
 
 class State(object):
@@ -101,10 +134,19 @@ class RecordingState(State):
         State.enter(self, machine)
         self.filename = create_file()
         self.future = time.monotonic() + RECORD_TIME
+	    self.data = []
 
     def exit(self, machine):
         State.exit(self, machine)
         self.filename = None
+        self.data = None
+        if self.filename:
+            write open("/{}.txt".format(self.filename), "a") as fp:
+                # Write Gyro Data
+	            for x in self.data:
+                    #fp.write("Accel X:%.2f Y:%.2f Z:%.2f ms^2 Gyro X:%.2f Y:%.2f Z:%.2f radians/s\r\n" % (sensor.acceleration + sensor.gyro))
+                    print(x)
+                    fp.write("Accel X:%.2f Y:%.2f Z:%.2f ms^2\r\n" % (sensor.acceleration))
 
     def update(self, machine):
         if State.update(self, machine):
@@ -113,13 +155,10 @@ class RecordingState(State):
                 machine.go_to_state('idle')
             if switch.fell:
                 machine.go_to_state('recording')
+            #if self.data ## something different needs to happen here
+            self.data.append(sensor.acceleration)
+            ## doesnt work very well ^^^
 
-            # If no conditions, write the file
-            if self.filename:
-                with open("/{}.txt".format(self.filename), "a") as fp:
-                    # Write Gyro Data
-                    fp.write("hello, world!\r\n")
-                
 ###### MAIN ######
 
 machine = StateMachine()
